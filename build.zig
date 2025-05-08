452e1e0653931cb5cb42a382576cb08cea768189
slushbox <kavorite@gmail.com>
2025-05-08 02:13:39 -0400

    add module

--- build.zig
+++ build.zig
@@ -7,6 +7,10 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    _ = b.addModule("chan", .{
+        .root_source_file = b.path("src/root.zig"),
+    });
+
     const lib = b.addStaticLibrary(.{
         .name = "chan",
         .root_source_file = b.path("src/root.zig"),
